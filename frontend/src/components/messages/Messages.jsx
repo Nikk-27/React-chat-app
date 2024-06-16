c8929f719e4d3e1ae9b9c0eb86400ac43c723afa
Nikk-27 <nktllwn@gmail.com>
2024-06-16 13:33:02 -0700

    chat functionality working

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -20,15 +20,15 @@ const Messages = () => {
 		<div className='px-4 flex-1 overflow-auto'>
 			{!loading &&
 				messages.length > 0 &&
-				messages.map((message) => (
-					<div key={message._id} ref={lastMessageRef}>
+				messages.map((message, idx) => (
+					<div key={message._id} ref={idx === messages.length - 1 ? lastMessageRef : null}>
 						<Message message={message} />
 					</div>
 				))}
 
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 			{!loading && messages.length === 0 && (
-				<p className='text-center'>Send a message to start the conversation</p>
+				<p className='text-center text-white opacity-70'>Send a message to start the conversation</p>
 			)}
 		</div>
 	);
